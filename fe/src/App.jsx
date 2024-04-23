8c437d107815e49b7707746eec07e4dd1c6dd144
Cesar Aguilar <Blueavian9@gmail.com>
2024-04-23 15:58:26 -0700

    updated commit

---
+++ fe/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+const initialData = [
+  {
+    fiz: "buz",
+    foo: "bar",
+    Baz: "biz";
+    buzResults: [
+      
+    ]
+  }
+];
+
+function App() {
+  const [theData, setTheData] = useState([]);
+  return (
+    <>
+      <h1>time2code</h1>
+    </>
+  );
+}
+
+export default App;
